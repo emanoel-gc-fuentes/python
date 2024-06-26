d364f24d30166bdabb660d3fc464c5d1b088b018
emanoel-gc-fuentes <emanoelgcfuentes@outlook.com>
2024-06-26 10:44:58 -0300

    add: modulo.py

---
+++ docs/introduction/modulo.py
@@ -0,0 +1,8 @@
+"""
+Módulo Python
+
+Um módulo em Python é um arquivo que contém definições e implementações de funções, 
+classes e variáveis, permitindo organizar e reutilizar código de forma eficiente. 
+Esses arquivos têm extensão .py, indicando ao interpretador Python que são módulos 
+que podem ser importados e utilizados em outros programas ou módulos.
+"""
